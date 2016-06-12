4cfd37655de8e0e7ffa4c2afc48cae6974d0341c
Andrew A Sanchez <andrewsanchez>
2016-06-12 15:53:18 -0700

    handle 7 levels of indentation

--- outline_parser.py
+++ outline_parser.py
@@ -6,7 +6,7 @@ from os import getcwd, path
 from numpy.random import rand
 
 def from_gdoc(file):
-    collapsed_name = file.strip(".txt")+"_collapsed.txt"
+    chunked_name = file.strip(".txt")+"_chunked.txt"
     lines = []
     with open(file) as outline:
         for line in outline:
@@ -19,12 +19,15 @@ def from_gdoc(file):
     p4 = re.compile("^ {9}\* ")
     p5 = re.compile("^ {12}\* ")
     p6 = re.compile("^ {15}\* ")
+    p7 = re.compile("^ {18}\* ")
 
     l1  = []
     l2  = []
     l3  = []
     l4  = []
     l5  = []
+    l6 = []
+    l7 = []
 
     cards = []
 
@@ -33,28 +36,39 @@ def from_gdoc(file):
             l1.append(line)
         elif p2.match(line):
             l2.append(line)
-            l2.insert(0, l1[-1])
-            l1.insert(-1, line)
         elif p3.match(line):
             l3.append(line)
-    #         cards.append((l1[-1], l2[-1], l3[-1]))
         elif p4.match(line):
             l4.append(line)
             if not p5.match(lines[lines.index(line)+1]):
                 cards.append((l1[-1], l2[-1], l3[-1], l4[-1]))
         elif p5.match(line):
             l5.append(line)
-            cards.append((l1[-1], l2[-1], l3[-1], l4[-1], l5[-1]))
+            try:
+                if not p6.match(lines[lines.index(line)+1]):
+                    cards.append((l1[-1], l2[-1], l3[-1], l4[-1], l5[-1]))
+            except:
+                continue
+        elif p6.match(line):
+            l6.append(line)
+            try:
+                if not p7.match(lines[lines.index(line)+1]):
+                    cards.append((l1[-1], l2[-1], l3[-1], l4[-1], l5[-1], l6[-1]))
+            except:
+                continue
+        elif p7.match(line):
+            l7.append(line)
+            cards.append((l1[-1], l2[-1], l3[-1], l4[-1], l5[-1], l6[-1], l7[-1]))
 
-    with open(collapsed_name, "a") as collapsed:
+    with open(chunked_name, "a") as chunked:
         for card in cards:
             flashcard = []
             for i in card[0:3]:
                 flashcard.append(i.strip())
             for x in card[3:]:
                 flashcard.append(x)
-            collapsed.write(" ".join(flashcard[0:3]) + "\n")
-            collapsed.write("".join(flashcard[3:]))
+            chunked.write(" ".join(flashcard[0:3]) + "\n")
+            chunked.write("".join(flashcard[3:]))
 
 def from_org(file):
     pass
@@ -64,13 +78,17 @@ def to_html(file):
     call(["pandoc", "-f", "org", "-t", "html", file, "-o", out_file])
 
 def html_to_anki(file):
+
+    html_file = file.replace(".txt", ".html")
+    call(["pandoc", "-f", "org", "-t", "html", file, "-o", html_file])
+
     replace_id = re.compile('<h1 id.*"')
     replace_h1 = re.compile(r"h1>")
     replace_newlines = re.compile(r"\n")
     h3 = re.compile('^"<h3')
     last_line_of_card = re.compile(r"</ul><br>")
 
-    with open(file) as in_file:
+    with open(html_file) as in_file:
         lines = list(in_file)
         in_file.close()
 
@@ -85,7 +103,7 @@ def html_to_anki(file):
             previous_line = lines[i-1]
             lines[i-1] = previous_line + '"'
 
-    out_file = file.replace(".html", ".csv")
+    out_file = html_file.replace(".html", ".csv")
     with open (out_file, "a") as out_file:
         card = []
         for line in lines:
@@ -100,11 +118,13 @@ def html_to_anki(file):
 def main():
     ap = argparse.ArgumentParser(description = "Turn your outlines in more manageable chunks of information.")
     ap.add_argument("file", help = "Path to the file you want to parse.")
-    ap.add_argument("-f", "--from", help = "Specify the format of your notes, i.e. gdocs or org")
-    ap.add_argument("-f", "--from", help = "Specify the format of your notes, i.e. gdocs or org")
-    ap.add_argument("-f", "--from", help = "Specify the format of your notes, i.e. gdocs or org")
+    ap.add_argument("-c", "--chunk", help = "Generate the chunked version of your outline.", action="store_true")
+    ap.add_argument("-a", "--anki", help = "Generate a .csv from HTML to import into Anki.", action="store_true")
     args = ap.parse_args()
 
-    from_gdoc(args.file)
+    if args.chunk:
+        from_gdoc(args.file)
+    elif args.anki:
+        html_to_anki(args.file)
 
 main()
